655dd0ac9ed0abbcf02a839c91f7d695d5bff933
Ted Cook <teodoro.cook@gmail.com>
2016-04-22 15:54:10 -0600

    Fixed test check

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 for i in *.template; do
   aws cloudformation validate-template --template-body file://$i;
-  if [ $? -neq 0 ]; then
+  if [ $? -ne 0 ]; then
     echo "Error validating file $i";
     exit 1;
   fi;
