a7627a5c3a5af56bd37e27291b7fb1321e1c96ce
Ted Cook <teodoro.cook@gmail.com>
2016-06-02 21:14:37 -0600

    Added network utility methods

--- lib/cfndsl-templates/networks.rb
+++ lib/cfndsl-templates/networks.rb
@@ -1,3 +1,28 @@
 require 'aws-sdk'
 
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 require_relative 'regions'
+=======
+=======
+>>>>>>> Stashed changes
+def aws_region_az()
+  aws_regions.inject({}) { |h, region|
+    ec2 = Aws::EC2::Client.new(region: region)
+    h[region] = ec2.describe_availability_zones.collect { |x| x.zone.name }
+    h
+  }
+end
+
+def region_az(region)
+    ec2 = Aws::EC2::Client.new(region: region)
+    ec2.describe_availability_zones.collect { |x| x.zone.name }
+end
+
+def aws_az()
+  aws_regions.flat_map { |x| region_az(x) }
+end
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
